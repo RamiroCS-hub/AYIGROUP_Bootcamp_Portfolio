e2fdc2315c2b9810f5022de46c66011958935d8e
Rama <ramirocarnicersouble8@gmail.com>
2024-08-29 22:07:27 -0300

    Ejercicio 1 - 29/08

---
+++ Ejercicios_Java_29.08/Ejercicio1/src/Main.java
@@ -0,0 +1,117 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.BreakIterator;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        run();
+    }
+
+    private static void run() throws IOException {
+        System.out.println("Ingrese el tamaño del vector");
+        int arraySize = readSize();
+
+        System.out.println("Ingrese los numeros que quieren que sean parte del vector");
+        int[] numbers = readArray(arraySize);
+
+        printArray(numbers);
+
+        int cantNarcicist = checkCantNarcicist(numbers);
+        System.out.printf("Ingresaste %d numeros narcicistas%n", cantNarcicist);
+
+        LinkedList<Integer> numbersList = convertToLinkedList(numbers);
+        System.out.printf("El size del Linked list es: %d%n", numbersList.size());
+
+        Queue<Integer> oddQueue = addOddToQueue(numbersList);
+        Stack<Integer> evenStack = addEvenToQueue(numbersList);
+
+        System.out.println("La cola de numeros impares contiene: ");
+        printList((List<Integer>) oddQueue);
+        System.out.println("La pila de numeros pares contiene: ");
+        printList(evenStack);
+    }
+
+    private static int readSize() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(br.readLine());
+    }
+
+    private static Stack<Integer> addEvenToQueue(LinkedList<Integer> numbersList) {
+        Stack<Integer> oddStack = new Stack<>();
+        numbersList.forEach(number -> {
+            if(isEven(number)) oddStack.push(number);
+        });
+        return oddStack;
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private static Queue<Integer> addOddToQueue(LinkedList<Integer> numbersList) {
+        Queue<Integer> oddQueque = new LinkedList<>();
+        numbersList.forEach(number -> {
+            if(!isEven(number)) oddQueque.add(number);
+        });
+        return oddQueque;
+    }
+
+    private static LinkedList<Integer> convertToLinkedList(int[] numbers) {
+        LinkedList<Integer> numbersList = new LinkedList<>();
+        for (int number : numbers) {
+            numbersList.add(number);
+        }
+        return numbersList;
+    }
+
+    private static int checkCantNarcicist(int[] numbers) {
+        int cantNarcicist = 0;
+        for(int i = 0; i < numbers.length; i++){
+           if(!isNarcissist(numbers[i]))
+               continue;
+           cantNarcicist++;
+        }
+        return cantNarcicist;
+    }
+
+    private static boolean isNarcissist(int number) {
+        char[] stringNumber = Integer.toString(number).toCharArray();
+        int result = 0;
+        for(char digit : stringNumber){
+            result += (int) Math.pow(Character.getNumericValue(digit), stringNumber.length);
+        }
+        return result == number;
+    }
+
+    private static int[] readArray(int arraySize) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = new int[arraySize];
+        String stringNumbers = sc.readLine();
+
+        BreakIterator bi = BreakIterator.getWordInstance();
+        bi.setText(stringNumbers);
+
+        int start = bi.first();
+        int counter = 0;
+        for(int end = start; end != BreakIterator.DONE; start = end, end = bi.next()) {
+            String word = stringNumbers.substring(start, end);
+            if(word.isBlank()) continue;
+            numbers[counter] = Integer.parseInt(word);
+            counter ++;
+        }
+        sc.close();
+        return numbers;
+    }
+
+    private static void printList(List<Integer> numbers) {
+        numbers.forEach(System.out::println);
+    }
+
+    private static void printArray(int[] array){
+        System.out.println("El array contiene los siguiente elementos: ");
+        Arrays.stream(array).forEach(System.out::println);
+    }
+}

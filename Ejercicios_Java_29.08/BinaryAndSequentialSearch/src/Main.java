31228b76b45d7e5f73eab0431364bcfee2ffb873
Rama <ramirocarnicersouble8@gmail.com>
2024-09-02 19:23:42 -0300

    Mismo array para las dos busquedas

--- Ejercicios_Java_29.08/BinaryAndSequentialSearch/src/Main.java
+++ Ejercicios_Java_29.08/BinaryAndSequentialSearch/src/Main.java
@@ -3,36 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+
+    static private final int VECTOR_SIZE = 10000;
+
     public static void main(String[] args) {
         try{
-           run();
+            int[] array = initializeArray();
+            Random random = new Random();
+            int numberToSearch = array[random.nextInt((array.length))];
+            run(array, numberToSearch);
         } catch (IOException e) {
             System.out.printf("Ocurrió un problema leyendo la consola: %s%n", e.getMessage());
         }catch (Exception e){
             System.out.printf("Ocurrió un problema corriendo el programa: %s%n", e.getMessage());
         }
+        //tryScanner();
     }
 
     private static int[] initializeArray(){
-       int[] array = new int[500];
+       int[] array = new int[VECTOR_SIZE];
        for(int i = 0; i < array.length; i++){
           array[i] = (int) (Math.random() * 100);
        }
        return array;
     }
 
-    private static void run() throws IOException {
-        int[] array = initializeArray();
+    private static void run(int[] array, int numberToSearch) throws IOException {
         System.out.printf("Ingrese que algoritmo de busqueda quiere utilizar %n [ 1 ] Secuencial %n [ 2 ] Binario %n");
         int choise = readNumber();
         /*
         System.out.println("Ingrese el numero que quiere buscar dentro del array");
         int numberToSearch = readNumber();
         */
-        Random random = new Random();
-        int numberToSearch = array[random.nextInt((array.length))];
         long startTime, endTime;
         switch (choise){
             case 1:
@@ -49,9 +54,18 @@ public class Main {
                 break;
             default:
                 System.out.println("Error tiene que ingresar alguno de las opciones provistas");
-                run();
+                run(array, numberToSearch);
         }
-        run();
+        run(array, numberToSearch);
+    }
+
+    private static void tryScanner(){
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(" ");
+        while(scanner.hasNext()){
+            System.out.println(scanner.next());
+        }
+        scanner.close();
     }
 
     private static void doBinarySearch(int[] array, int numberToSearch){

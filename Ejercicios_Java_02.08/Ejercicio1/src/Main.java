a01b440b4acfe2dbca9a726604d732149379ab8c
Rama <ramirocarnicersouble8@gmail.com>
2024-09-03 21:54:37 -0300

    Numeros de Keplekar y Narcicistas agregados

--- Ejercicios_Java_02.08/Ejercicio1/src/Main.java
+++ Ejercicios_Java_02.08/Ejercicio1/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.logging.SocketHandler;
 
 public class Main {
@@ -7,7 +8,7 @@ public class Main {
         try{
             run();
         } catch (Exception e) {
-            System.out.printf("Ocurrió en el programa %s%n", e.getMessage());
+            System.out.printf("Ocurrió un error en el programa %s%n", e.getMessage());
         }
     }
 
@@ -26,13 +27,125 @@ public class Main {
         int[] sortedArray = sortArray(array);
         System.out.println("El array ordenado de menor a mayor es: ");
         Arrays.stream(sortedArray).forEach(System.out::println);
+
+        LinkedList<Integer> arrayList = cloneArrayToList(sortedArray);
+        sortedArray = Arrays.stream(sortedArray).map(number -> number = 0).toArray().clone();
+        Arrays.stream(sortedArray).forEach(System.out::println);
+
+        System.out.println("La linked list es: ");
+        arrayList.forEach(System.out::println);
+        arrayList = (LinkedList<Integer>) deleteRepeatedNumbers(arrayList).clone();
+
+        System.out.println("La linked list sin valores repetidos es: ");
+        arrayList.forEach(System.out::println);
+
+        int cantNarcicist = getCantNarcicist(arrayList);
+        System.out.println("La cantidad de numeros narcicist en el array es: " + cantNarcicist);
+
+        System.out.println("Los numeros de Keprekar en la lista son: ");
+        showKeprekarNumbers(arrayList);
+    }
+
+    private static void showKeprekarNumbers(LinkedList<Integer> arrayList) {
+        for (int i : arrayList) {
+            if(isKeprekar(i))
+                System.out.println(i);
+        }
+    }
+
+    private static boolean isKeprekar(int number) {
+        int[] numberArray = fromIntToIntArray(number);
+        int numberKeprekared = getKeprekarNumber(numberArray);
+        return numberKeprekared == number;
+    }
+
+    private static int getKeprekarNumber(int[] numberArray) {
+        int sum = 0;
+        for(int i : numberArray){
+           sum += (int) Math.pow(i, 2);
+        }
+        return sum;
+    }
+
+    private static int getCantNarcicist(LinkedList<Integer> arrayList) {
+        int counter = 0;
+        for(int i : arrayList) {
+           if(isNarcicist(i))
+               counter++;
+        }
+        return counter;
+    }
+
+    private static boolean isNarcicist(int n) {
+        int[] numberArray = fromIntToIntArray(n);
+        int numberNarcicisted = getNarcicist(numberArray);
+        return numberNarcicisted == n;
+    }
+
+    private static int[] fromIntToIntArray(int n) {
+        char[] number = getCharArray(n);
+        return charArrayToInt(number);
+    }
+
+    private static char[] getCharArray(int n){
+        String nString = String.valueOf(n);
+        char[] number = nString.toCharArray();
+        return number;
+    }
+
+    private static int getNarcicist(int[] numberArray) {
+        int sum = 0;
+        for(int i : numberArray) {
+           sum += (int) Math.pow(i, numberArray.length);
+        }
+        return sum;
+    }
+
+    private static int[] charArrayToInt(char[] number){
+        int[] numberArray = new int[number.length];
+        for (int h = 0; h < number.length; h++) {
+            numberArray[h] = getIntFromChar(number[h]);
+        }
+        return numberArray;
+    }
+
+    private static int getIntFromChar(char d){
+       return Integer.parseInt(String.valueOf(d));
+    }
+
+    private static LinkedList<Integer> deleteRepeatedNumbers(LinkedList<Integer> arrayList) {
+        LinkedList<Integer> newList = (LinkedList<Integer>) arrayList.clone();
+        for(int i : arrayList) {
+           for(int j = 1; j < arrayList.size(); j++) {
+               if(arrayList.indexOf(i) == j)
+                   continue;
+
+               if(i == arrayList.get(j)) {
+                   newList.remove(arrayList.indexOf(i));
+               }
+           }
+        }
+        return newList;
+    }
+
+    private static LinkedList<Integer> cloneArrayToList(int[] array) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        return list;
     }
 
     private static int[] getArray(int size) {
         int[] array = new int[size];
         Random random = new Random();
         for(int i=0; i<size; i++){
-           array[i] = (random.nextInt(1, 500));
+           int number = (random.nextInt(1, 10));
+           if(verifyNumber(Integer.toString(number))){
+               i--;
+               continue;
+            }
+           array[i] = number;
         }
         return array;
     }
@@ -59,7 +172,7 @@ public class Main {
         try {
             System.out.printf("Introduce %d numeros%n", size);
             for(int i = 0; i < size; i++) {
-                array[i] = verifyNumber(sc.next());
+                //array[i] = verifyNumber(sc.next());
                 System.out.println(sc.next());
             }
         } catch (IllegalArgumentException e){
@@ -72,12 +185,9 @@ public class Main {
         return array;
     }
 
-    private static int verifyNumber(String number) {
+    private static boolean verifyNumber(String number) {
         int numberInt = Integer.parseInt(number);
-        if(numberInt < 0){
-            throw new IllegalArgumentException("ELos valores del array deben de ser mayores o iguales a 0");
-        }
-        return numberInt;
+        return numberInt < 0;
     }
 
     private static int readNumber() {
